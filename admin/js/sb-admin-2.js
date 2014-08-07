d1a2a1ed4dec2c4335693b9db1df352a03a64950
Varinder Singh <highpolyboy@gmail.com>
2014-08-07 15:44:13 +1200

    Added a broken thing

--- admin/js/sb-admin-2.js
+++ admin/js/sb-admin-2.js
@@ -3,11 +3,11 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
-//Loads the correct sidebar on window load,
+//Loads the correct sidebar on window load
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).bind("load resize", function() {
+    $(window).bind("load resize"
         var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
